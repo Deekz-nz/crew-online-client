da478a39ea735426113160a02cbc32915856d593
Daniel Kahu <daniel@kahu.org>
2025-03-22 13:11:20 +1300

    serve backend url via environment variable

--- src/hooks/GameProvider.tsx
+++ src/hooks/GameProvider.tsx
@@ -2,9 +2,7 @@ import { createContext, useContext } from "react";
 import { useGameRoom } from "../hooks/useGameRoom"; // Move hook here
 import * as Colyseus from "colyseus.js";
 
-const client = new Colyseus.Client("ws://localhost:2567");
 
-const GameContext = createContext<ReturnType<typeof useGameRoom> | null>(null);
 
 /**
  * GameProvider (React Context)
@@ -18,6 +16,10 @@ const GameContext = createContext<ReturnType<typeof useGameRoom> | null>(null);
  * Allows any component to access game state without prop drilling.
  * Must wrap the app in <GameProvider> in App.tsx.
  */
+
+const client = new Colyseus.Client(import.meta.env.VITE_COLYSEUS_URL);
+
+const GameContext = createContext<ReturnType<typeof useGameRoom> | null>(null);
 
 export const GameProvider = ({ children }: { children: React.ReactNode }) => {
   const game = useGameRoom(client);
