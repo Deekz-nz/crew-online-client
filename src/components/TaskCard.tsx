00e89e791475b161316f2285b14bcc5be2b245bd
Daniel Kahu <daniel@kahu.org>
2025-06-28 14:52:42 +1200

    make token slightly bigger

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -31,7 +31,7 @@ export const TaskCard: React.FC<TaskCardProps> = ({
   const { color, number } = card;
   const { background, dark } = colorStyles[color];
 
-  const tokenHeight = width * 0.3; // circle token size
+  const tokenHeight = width * 0.6; // circle token size
   const tokenGap = 4;
   const cardHeight = width * 1.1;
   const wrapperHeight = tokenHeight + tokenGap + cardHeight;
@@ -73,7 +73,7 @@ export const TaskCard: React.FC<TaskCardProps> = ({
           display: 'flex',
           justifyContent: 'center',
           alignItems: 'center',
-          fontSize: width * 0.15,
+          fontSize: width * 0.3,
           border: circleContent ? '2px solid black' : 'none',
           boxShadow: circleContent ? '0 0 4px rgba(0, 0, 0, 0.4)' : 'none',
         }}
